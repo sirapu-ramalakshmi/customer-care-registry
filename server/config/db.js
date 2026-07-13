7b4a5b37786b38d8700dd09981b6ed30474ef8c7
ramalakshmisirapu38-cpu <ramalakshmisirapu38@gmail.com>
2026-07-13 21:28:16 +0530

    Ready for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("Connecting to MongoDB...");
+    console.log(process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB Connected");
-  } catch (error) {
-    console.log("❌ Database Connection Error:", error.message);
+  } catch (err) {
+    console.error(err);
     process.exit(1);
   }
 };
--- server/server.js
+++ server/server.js
@@ -32,4 +32,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
+  console.log("MONGO_URI =", process.env.MONGO_URI);
 });
